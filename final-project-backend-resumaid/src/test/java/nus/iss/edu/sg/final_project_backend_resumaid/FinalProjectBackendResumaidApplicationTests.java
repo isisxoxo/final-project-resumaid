777c48ffe15aa365ad705c8439cb63b703d3771b
Isis <isislee107@gmail.com>
2024-06-17 00:44:22 +0800

    first commit

---
+++ final-project-backend-resumaid/src/test/java/nus/iss/edu/sg/final_project_backend_resumaid/FinalProjectBackendResumaidApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.iss.edu.sg.final_project_backend_resumaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectBackendResumaidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
